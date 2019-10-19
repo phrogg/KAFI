b22bfc1a53cb522e710fa8b424174532305fb7e2
Phil Roggenbuck <ceo@roggstar.eu>
2019-10-19 09:34:15 +0200

    Update injectme.js

--- injectme.js
+++ injectme.js
@@ -1,11 +1,14 @@
 var verk = document.getElementsByClassName("text-bold text-bigger text-force-linebreak")[0].firstElementChild.innerHTML.replace("\n                                        ","");
-
 var ware = document.getElementById("viewad-title").innerHTML.replace("\n                                    ","");
-
 var kauf = document.getElementsByClassName("formcontrol viewad-contact-contactName")[0].value;
+var mess = document.getElementsByClassName("viewad-contact-message");
 
-var mess = document.getElementsByClassName("viewad-contact-message")[0];
+var nwstyle = "margin-top: 0px; margin-bottom: 0px; height: 200px;";
 
-mess.style = "margin-top: 0px; margin-bottom: 0px; height: 200px;";
+var nwmesag = "Sehr geehrter Herr/Frau "+verk+",\n\nIch interessiere mich für "+ware+". Und wollte fragen \n\nLiebe Grüße,\n"+kauf;
 
-mess.value = "Sehr geehrter Herr/Frau "+verk+",\n\nIch interessiere mich für "+ware+". Und wollte fragen \n\nLiebe Grüße,\n"+kauf;
+mess[0].style = nwstyle;
+mess[1].style = nwstyle;
+
+mess[0].value = nwmesag;
+mess[1].value = nwmesag;
