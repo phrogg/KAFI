84b166268b8ffda8f6b3bf63c9dcb5e3247dc1cd
Phil Roggenbuck <ceo@roggstar.eu>
2019-10-19 09:54:17 +0200

    Upper cased the first character of the name

--- injectme.js
+++ injectme.js
@@ -3,6 +3,8 @@ var ware = document.getElementById("viewad-title").innerHTML.replace("\n        
 var kauf = document.getElementsByClassName("formcontrol viewad-contact-contactName")[0].value;
 var mess = document.getElementsByClassName("viewad-contact-message");
 
+verk = verk[0].toUpperCase() + verk.substring(1,verk.length);
+
 var nwstyle = "margin-top: 0px; margin-bottom: 0px; height: 200px;";
 
 var nwmesag = "Sehr geehrter Herr/Frau "+verk+",\n\nIch interessiere mich für "+ware+". Und wollte fragen \n\nLiebe Grüße,\n"+kauf;
